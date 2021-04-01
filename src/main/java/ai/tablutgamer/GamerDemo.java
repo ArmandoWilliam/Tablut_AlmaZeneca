bd83b015c06c243191aaf07bf06e0791f4aa9bb3
davidetazzioli <davidetazzioli@localhost>
2021-04-01 19:14:11 +0200

    creata classe main "GamerDemo" nel file source src/main/java/ai/tablutgamer.

---
+++ src/main/java/ai/tablutgamer/GamerDemo.java
@@ -0,0 +1,11 @@
+package ai.tablutgamer;
+
+public class GamerDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("sono la demo del giocatore di tablut!");
+
+	}
+
+}
