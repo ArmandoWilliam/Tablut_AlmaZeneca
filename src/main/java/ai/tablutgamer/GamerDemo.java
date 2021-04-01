e14cf81a6215caed7cf5951082ec77d89d46825e
davidetazzioli <davidetazzioli@localhost>
2021-04-01 19:18:35 +0200

    aggiunte delle cose a caso per fare una prova

--- src/main/java/ai/tablutgamer/GamerDemo.java
+++ src/main/java/ai/tablutgamer/GamerDemo.java
@@ -5,6 +5,10 @@ public class GamerDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("sono la demo del giocatore di tablut!");
+		System.out.println("ancora non so fare niente");
+		
+		int a;
+		int b;
 
 	}
 
