a427daeddefe6923094858a02273d96bf0d55cd1
armi- <armi-@DESKTOP-H61E76C>
2021-04-30 18:07:54 +0200

    Modified BlackHeuristics

--- src/it/unibo/ai/didattica/competition/tablut/almazeneca/heuristics/BlackHeuristics.java
+++ src/it/unibo/ai/didattica/competition/tablut/almazeneca/heuristics/BlackHeuristics.java
@@ -21,7 +21,40 @@ public class BlackHeuristics {
 
 	public double evaluate(State state) {
 		
-		return numBlackPawn;
+		//valore valutazione
+		double result=0;
+		result+= WEIGHT_BLACK_PAWN*this.numBlackPawn;
+		result+=WEIGHT_WHITE_PAWN*this.numWhitePawn;
+		result+=WEIGHT_KING_POS*this.kingPosition;
 		
+		return result;
+		
+		
+	}
+	
+	
+	
+	public int[] getKingPosition() {
+		
+		int[] kingPosition = {};
+		
+		for(int i=0; i<9; i++) {
+			for(int z=0; z<9; z++) {
+				if(state.getBox(i, z).equals("K")) {
+					kingPosition[0]=i;
+					kingPosition[1]=z;
+					return kingPosition;
+				}
+			}
+		}
+		return kingPosition;
+	}
+
+
+
+	public double evalKingPosition() {
+		if(kingPosition + [0][1]) {
+			
+		}
 	}
 }
