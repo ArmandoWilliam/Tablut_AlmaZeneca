69c609faaf516b5872348ea4f29cfb1bfbca4e82
armi- <armi-@DESKTOP-H61E76C>
2021-05-01 09:22:14 +0200

    commented evalKingPosition

--- src/it/unibo/ai/didattica/competition/tablut/almazeneca/heuristics/BlackHeuristics.java
+++ src/it/unibo/ai/didattica/competition/tablut/almazeneca/heuristics/BlackHeuristics.java
@@ -52,9 +52,9 @@ public class BlackHeuristics {
 
 
 
-	public double evalKingPosition() {
-		if(kingPosition + [0][1]) {
-			
-		}
-	}
+//	public double evalKingPosition() {
+//		if(kingPosition + [0][1]) {
+//			
+//		}
+//	}
 }
